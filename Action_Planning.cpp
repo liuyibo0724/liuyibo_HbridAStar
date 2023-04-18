33c5892c0a42a178fe6d3eec13ac5831754c8c2e
liuyibo <392428465@qq.com>
2023-04-18 18:27:59 +0800

    进行AP模块封装

--- Action_Planning.cpp
+++ Action_Planning.cpp
@@ -2,13 +2,99 @@
 
 using namespace actionPlanning;
 
+ActionPlanner::ActionPlanner() {}
+
+ActionPlanner::~ActionPlanner() { delete m_planner; } //析构函数
+
 bool ActionPlanner::init(unsigned char* data, int width, int height)
 {
     m_planner = new HybridAStar::planner(data, width, height);
     return true;
 }
 
-bool ActionPlanner::run()
+bool ActionPlanner::plan()
 {
-    
+    m_planner->setStart(m_sta_goa.first);
+    m_planner->setGoal(m_sta_goa.second);
+    m_planner->plan();
+    isRunSuccess = true;
+    return true;
 }
+
+bool ActionPlanner::setStartGoal(HybridAStar::Node3D start, HybridAStar::Node3D goal)
+{
+    m_sta_goa.first = start;
+    m_sta_goa.second = goal;
+    return true;
+}
+
+void ActionPlanner::whichNode(std::vector<HybridAStar::Node3D> &BSplinePathFirst)
+{
+    float x = localization.longitude;
+    float y = localization.latitude;
+    float t = localization.headingAngle;
+    if(pathIdx == -1)
+    {
+        if(abs(x - BSplinePathFirst[0].getX()) < param::tieBreaker &&
+           abs(y - BSplinePathFirst[0].getY()) < param::tieBreaker &&
+           (abs(t - BSplinePathFirst[0].getT()) < param::deltaHeadingRad || 
+            abs(t - BSplinePathFirst[0].getT()) > param::deltaHeadingNegRad))
+            pathIdx = 0;
+        else pathIdx = -1;
+    }
+    else
+    {
+        float disSquaSelf = pow(x - BSplinePathFirst[pathIdx].getX(), 2.) +
+                            pow(y - BSplinePathFirst[pathIdx].gety(), 2.) +
+                            pow(t - BSplinePathFirst[pathIdx].getT(), 2.);
+        float disSquaSucc;
+        if(pathIdx < BSplinePathFirst.size() - 1)
+            disSquaSucc = pow(x - BSplinePathFirst[pathIdx + 1].getX(), 2.) +
+                          pow(y - BSplinePathFirst[pathIdx + 1].gety(), 2.) +
+                          pow(t - BSplinePathFirst[pathIdx + 1].getT(), 2.);
+        else disSquaSucc = 1.f;
+
+        if(disSquaSelf > 1.f || disSquaSucc > 1.f) pathIdx = -1;
+        else if(disSquaSelf <= disSquaSucc) pathIdx = pathIdx;
+        else pathIdx += 1;
+    }
+}
+
+bool ActionPlanner::DDSMsgsOut()
+{
+    if(!isRunSuccess) return false;
+    auto firstBSplinePath = *(m_planner->getBSplinePath().begin());
+    whichNode(firstBSplinePath.first);  //找到哪个点匹配当前位置
+    outputDT.seq = pathIdx;             //赋值序列seq
+    if(pathIdx == -1)
+    {
+        isRunSuccess = false;
+        m_sta_goa.first = HybridAStar::Node3D(localization.longitude, localization.latitude, localization.headingAngle, 0, 0, nullptr,);    //更新起始点
+        plan();     //重新规划
+        firstBSplinePath = *(m_planner->getBSplinePath().begin());
+        outputDT.isPlanSucceeded = isRunSuccess;
+        for(int i = 0; i < firstBSplinePath.size(); i ++)
+        {
+            outputDT.discretizedTrajectory[i].pathPoint.x = firstBSplinePath[i].first.getX();
+            outputDT.discretizedTrajectory[i].pathPoint.y = firstBSplinePath[i].first.getY();
+            outputDT.discretizedTrajectory[i].pathPoint.kappa = firstBSplinePath[i].first.getT();
+            outputDT.discretizedTrajectory[i].Vel = firstBSplinePath[i].first.getX();
+        }
+        outputDT.stamp = sysd::now();
+        whichNode(firstBSplinePath.first);  //找到哪个点匹配当前位置
+        outputDT.seq = pathIdx;             //赋值序列seq
+    }
+    return true;
+}
+
+void ActionPlanner::getInputMsgs(ActionPlannerInfo     &CurrentAPInfo,
+                                 EgoVehicleCanInfoMsg  &InputEVCanInfoMsg,
+                                 Localization          &Localization)
+{
+    currentAPInfo = CurrentAPInfo;
+    inputEVCanInfoMsg = InputEVCanInfoMsg;
+    localization = Localization;
+}
+
+void ActionPlanner::getOutputDT(DiscretizedTrajectory &OutputDT)
+{ OutputDT = OutputDT; }
--- Action_Planning.hpp
+++ Action_Planning.hpp
@@ -2,7 +2,9 @@
 #define ACTION_PLANNING_HPP
 
 #include <stdlib.h>
+#include <vector>
 #include "includes/planner.h"
+#include "AP_interfaceProcess.hpp"
 
 namespace actionPlanning
 {
@@ -10,12 +12,29 @@ namespace actionPlanning
     class ActionPlanner
     {
     public:
-        ActionPlanner() {}  //构造函数
-        ~ActionPlanner() {} //析构函数
-        bool init(unsigned char* data, int width, int height);        //初始化
-        bool run();   //运行
-    private:
-        HybridAStar::planner *m_planner;
+        ActionPlanner() {}                                                       //构造函数
+        ~ActionPlanner() {}                                                      //析构函数
+        bool init(unsigned char* data, int width, int height);                   //初始化
+        bool setStartGoal(HybridAStar::Node3D start, HybridAStar::Node3D goal); 
+        bool plan();                                                             //规划
+        void whichNode(std::vector<HybridAStar::Node3D> &BSplinePathFirst);      //查找当前位置是在那个点上
+        bool DDSMsgsOut();                                                       //将规划结果转换成DDS发布格式
+        bool run();                                                              //运行核心函数
+        void getInputMsgs(ActionPlannerInfo     &CurrentAPInfo,
+                          EgoVehicleCanInfoMsg  &InputEVCanInfoMsg,
+                          Localization          &Localization)                   //传入所需信号
+        void getOutputDT(DiscretizedTrajectory &OutputDT);                       //传出轨迹点列输出信号
+    private: 
+        HybridAStar::planner                                *m_planner;          //私有规划器
+        std::pair<HybridAStar::Node3D, HybridAStar::Node3D> m_sta_goa;           //起始点和目标点
+        bool                                                isRunSuccess = false;//是否规划运行成功
+        int                                                 pathIdx = -1;        //当前所在路径点的序号（注意！！！到时候需要和outputDT一起发布）
+ 
+        ActionPlannerInfo                                   currentAPInfo;       //当前运动规划器状态信息
+        EgoVehicleCanInfoMsg                                inputEVCanInfoMsg;   //输入的自车Can信息
+        Localization                                        localization;        //输入的定位信息
+ 
+        DiscretizedTrajectory                               outputDT;            //输出轨迹点列
     };
 }
 
