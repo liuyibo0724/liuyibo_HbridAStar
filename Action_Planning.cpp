4994f92ace9585a1d77a3fc36db46634d5884b75
liuyibo <392428465@qq.com>
2023-04-28 13:43:43 +0800

    基本解决转向角涉及间端点的问题

--- Action_Planning.cpp
+++ Action_Planning.cpp
@@ -48,12 +48,10 @@ void ActionPlanner::whichNode(std::vector<HybridAStar::Node3D> &BSplinePathFirst
     //若超出轨迹线条外pathIdx转为-1
     else if(pathIdx == BSplinePathFirst.size() - 1)
     {
-        if((pow(x - BSplinePathFirst[pathIdx - 1].getX(), 2.) +
-            pow(y - BSplinePathFirst[pathIdx - 1].gety(), 2.) +
-            pow(t - BSplinePathFirst[pathIdx - 1].getT(), 2.)) >
-           (pow(BSplinePathFirst[pathIdx].getX() - BSplinePathFirst[pathIdx - 1].getX(), 2.) +
-            pow(BSplinePathFirst[pathIdx].getY() - BSplinePathFirst[pathIdx - 1].gety(), 2.) +
-            pow(BSplinePathFirst[pathIdx].getT() - BSplinePathFirst[pathIdx - 1].getT(), 2.) + param::tieBreaker)) pathIdx = -1;
+        if(abs(x - BSplinePathFirst[pathIdx].getX()) > param::tieBreaker ||
+           abs(y - BSplinePathFirst[pathIdx].getY()) > param::tieBreaker ||
+           (abs(t - BSplinePathFirst[pathIdx].getT()) > param::deltaHeadingRad && 
+            abs(t - BSplinePathFirst[pathIdx].getT()) < param::deltaHeadingNegRad)) pathIdx = -1;
     }
     //其余情况靠近本点pathIdx即为本点靠近下一点即为下一点，若距离两点均太远置为-1
     else
@@ -65,9 +63,12 @@ void ActionPlanner::whichNode(std::vector<HybridAStar::Node3D> &BSplinePathFirst
                             pow(y - BSplinePathFirst[pathIdx + 1].gety(), 2.) +
                             pow(t - BSplinePathFirst[pathIdx + 1].getT(), 2.);
 
-        if(disSquaSelf > 1.f && disSquaSucc > 1.f) pathIdx = -1;
-        else if(disSquaSelf <= disSquaSucc) pathIdx = pathIdx;
-        else pathIdx += 1;
+        if(disSquaSelf > 2.f && disSquaSucc > 2.f) pathIdx = -1;
+
+        else if(abs(x - BSplinePathFirst[pathIdx + 1].getX()) < param::tieBreaker &&
+           abs(y - BSplinePathFirst[pathIdx + 1].getY()) < param::tieBreaker &&
+           (abs(t - BSplinePathFirst[pathIdx + 1].getT()) < param::deltaHeadingRad || 
+            abs(t - BSplinePathFirst[pathIdx + 1].getT()) > param::deltaHeadingNegRad)) pathIdx ++;
     }
 }
 
@@ -95,41 +96,55 @@ bool ActionPlanner::DDSMsgsOut()
             outputDT.discretizedTrajectory[i].pathPoint.kappa = firstBSplinePath[i].first.getT();
             outputDT.discretizedTrajectory[i].Vel = firstBSplinePath[i].second;
         }
-        for(int i = 0; i < firstBSplinePath.size(); i ++)   //赋值车轮转向角theta
+        //利用isCusp函数查找所有分段点
+        std::vector<int> subSegs;
+        for(int i = 0; i < firstBSplinePath.size(); i ++) if(HybridAStar::isCusp(firstBSplinePath, i)) subSegs.push_back(i);
+        //分别处理每个子段，防止分段点的影响，每个字段左闭右开
+        for(int j = 0; j < subSegs.size() - 1; j ++)
         {
-            int lowerIdx = std::max(0, i - 1);                              //带边界限制的下指标
-            int upperIdx = std::min(firstBSplinePath.size() - 1, i + 1);    //带边界限制的上指标
-            //前后线段中点分别设为P0和P1
-            //P0数据
-            float P0_x = (outputDT.discretizedTrajectory[lowerIdx].pathPoint.x +
-                          outputDT.discretizedTrajectory[i].pathPoint.x) / 2.f;
-            float P0_y = (outputDT.discretizedTrajectory[lowerIdx].pathPoint.y +
-                          outputDT.discretizedTrajectory[i].pathPoint.y) / 2.f;
-            float P0_kappa = (outputDT.discretizedTrajectory[lowerIdx].pathPoint.kappa +
-                              outputDT.discretizedTrajectory[i].pathPoint.kappa) / 2.f;
-            float P0_Vel = (outputDT.discretizedTrajectory[lowerIdx].Vel +
-                            outputDT.discretizedTrajectory[i].Vel) / 2.f;
-            //P1数据
-            float P1_x = (outputDT.discretizedTrajectory[upperIdx].pathPoint.x +
-                          outputDT.discretizedTrajectory[i].pathPoint.x) / 2.f;
-            float P1_y = (outputDT.discretizedTrajectory[upperIdx].pathPoint.y +
-                          outputDT.discretizedTrajectory[i].pathPoint.y) / 2.f;
-            float P1_kappa = (outputDT.discretizedTrajectory[upperIdx].pathPoint.kappa +
-                              outputDT.discretizedTrajectory[i].pathPoint.kappa) / 2.f;
-            float P1_Vel = (outputDT.discretizedTrajectory[upperIdx].Vel +
-                            outputDT.discretizedTrajectory[i].Vel) / 2.f;
-            
-            float delta_kappa = P1_kappa - P0_kappa;
-            float P1_P0_length = sqrt(pow(P1_x - P0_x, 2.f) + pow(P1_y - P0_y, 2.f));
-            float delta_time = P1_P0_length / outputDT.discretizedTrajectory[i].Vel;
+            int sta = subSegs[j], end = subSegs[j + 1];
+            for(int i = sta; i < end; i ++)                                     //赋值车轮转向角theta, Acc, Jerk
+            {
+                int lowerIdx = std::max(sta, i - 1);                            //带边界限制的下指标
+                int upperIdx = std::min(end - 1, i + 1);                        //带边界限制的上指标
+                //前后线段中点分别设为P0和P1
+                //P0数据
+                float P0_x = (outputDT.discretizedTrajectory[lowerIdx].pathPoint.x +
+                              outputDT.discretizedTrajectory[i].pathPoint.x) / 2.f;
+                float P0_y = (outputDT.discretizedTrajectory[lowerIdx].pathPoint.y +
+                              outputDT.discretizedTrajectory[i].pathPoint.y) / 2.f;
+                float P0_kappa = (outputDT.discretizedTrajectory[lowerIdx].pathPoint.kappa +
+                                  outputDT.discretizedTrajectory[i].pathPoint.kappa) / 2.f;
+                float P0_Vel = (outputDT.discretizedTrajectory[lowerIdx].Vel +
+                                outputDT.discretizedTrajectory[i].Vel) / 2.f;
+                //P1数据
+                float P1_x = (outputDT.discretizedTrajectory[upperIdx].pathPoint.x +
+                              outputDT.discretizedTrajectory[i].pathPoint.x) / 2.f;
+                float P1_y = (outputDT.discretizedTrajectory[upperIdx].pathPoint.y +
+                              outputDT.discretizedTrajectory[i].pathPoint.y) / 2.f;
+                float P1_kappa = (outputDT.discretizedTrajectory[upperIdx].pathPoint.kappa +
+                                  outputDT.discretizedTrajectory[i].pathPoint.kappa) / 2.f;
+                float P1_Vel = (outputDT.discretizedTrajectory[upperIdx].Vel +
+                                outputDT.discretizedTrajectory[i].Vel) / 2.f;
 
-            float theta = atan2(delta_kappa * param::alxesDis, P1_P0_length);   //注意！！！此theta值取值范围为[-M_PI/2, M_PI/2]
-            outputDT.discretizedTrajectory[i].pathPoint.theta = theta;
+                float delta_kappa = P1_kappa - P0_kappa;
+                float P1_P0_length = sqrt(pow(P1_x - P0_x, 2.f) + pow(P1_y - P0_y, 2.f));
+                float delta_time = P1_P0_length / outputDT.discretizedTrajectory[i].Vel;
 
-            float Acc = (P1_Vel - P0_Vel) / delta_time;
-            outputDT.discretizedTrajectory[i].Acc = Acc;
-            outputDT.discretizedTrajectory[i].Jerk = 0.;
+                float theta = atan2(delta_kappa * param::alxesDis, P1_P0_length);   //注意！！！此theta值取值范围为[-M_PI/2, M_PI/2]
+                outputDT.discretizedTrajectory[i].pathPoint.theta = theta;
+
+                float Acc = (P1_Vel - P0_Vel) / delta_time;
+                outputDT.discretizedTrajectory[i].Acc = Acc;
+                outputDT.discretizedTrajectory[i].Jerk = 0.;
+            }
         }
+        //终末点Vel, theta, Acc, Jerk全部赋值为0
+        outputDT.discretizedTrajectory[firstBSplinePath.size() - 1].Vel = 0.;
+        outputDT.discretizedTrajectory[firstBSplinePath.size() - 1].pathPoint.theta = 0.;
+        outputDT.discretizedTrajectory[firstBSplinePath.size() - 1].Acc = 0.;
+        outputDT.discretizedTrajectory[firstBSplinePath.size() - 1].Jerk = 0.;
+
         outputDT.stamp = sysd::now();
         whichNode(firstBSplinePath.first);  //找到哪个点匹配当前位置
         outputDT.seq = pathIdx;             //赋值序列seq
